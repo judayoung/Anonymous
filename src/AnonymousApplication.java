1249165d82c8fcc07fb16c93c7effd1900296d42
Ju Dayoung <ekdud7703@naver.com>
2022-09-28 19:20:05 +0900

    소소한 수정 : 익명구현객체를 람다로

--- src/AnonymousApplication.java
+++ src/AnonymousApplication.java
@@ -1,5 +1,7 @@
 import anonymous.Anonymous;
 
+import java.util.function.IntBinaryOperator;
+
 public class AnonymousApplication {
 
     public static void main(String[] args) {
@@ -13,10 +15,9 @@ public class AnonymousApplication {
                 for (int i = msg.length() - 1; i >= 0; i--) {
                     resultMsg.append(msg.charAt(i));
                 }
-                return "aaaEncryptAnonymous : " + resultMsg.toString();
+                return "aaaEncryptAnonymous : " + resultMsg;
             }
         };
-
         System.out.println(aaaEncryptAnonymous.encryptMsg("Hello world!"));
 
         Anonymous bbbEncryptAnonymous = new Anonymous() {
@@ -39,11 +40,31 @@ public class AnonymousApplication {
                         default: resultMsg.append(string); break;
                     }
                 }
-                return "bbbEncryptAnonymous : " + resultMsg.toString();
+                return "bbbEncryptAnonymous : " + resultMsg;
             }
         };
-
         System.out.println(bbbEncryptAnonymous.encryptMsg("Hello world!"));
+
+        Anonymous cccEncryptAnonymous = msg -> {
+            StringBuilder resultMsg = new StringBuilder();
+            for (int i = 0; i < msg.length(); i++) {
+                String string = Character.toString(msg.charAt(i)).toLowerCase();
+                switch (string) {
+                    case "a" -> resultMsg.append("1");
+                    case "b" -> resultMsg.append("2");
+                    case "c" -> resultMsg.append("3");
+                    case "d" -> resultMsg.append("4");
+                    case "e" -> resultMsg.append("5");
+                    case "f" -> resultMsg.append("6");
+                    case "g" -> resultMsg.append("7");
+                    case "h" -> resultMsg.append("8");
+                    case "i" -> resultMsg.append("9");
+                    default -> resultMsg.append(string);
+                }
+            }
+            return "bbbEncryptAnonymous : " + resultMsg;
+        };
+        System.out.println(cccEncryptAnonymous.encryptMsg("Hello world!"));
     }
 
 }
